aaf7b292bd0cc20d948cf8ad419b9dba1a42d7b2
ifotn <rich@infrontofthenet.com>
2017-12-21 12:15:14 -0500

    end of hour 1.  api setup

---
+++ MusicStoreAPI/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStoreAPI.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ MusicStoreAPI/Models/MusicStoreModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStoreAPI.Models
+{
+    public class MusicStoreModel : DbContext
+    {
+        // constructor
+        public MusicStoreModel(DbContextOptions<MusicStoreModel> options) : base(options)
+        {
+
+        }
+    }
+}
